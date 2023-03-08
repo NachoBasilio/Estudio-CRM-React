7a931e0596389040eb8662d868a985f02af664df
IgnaDev <arcortoon@gmail.com>
2023-03-08 19:37:41 -0300

    start: configurando mi primire router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Inicio</h1>
+  },
+  {
+    path: '/nosotros',
+    element: <p>Nosotros</p>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
