7226710e5ec5709d4ccf4a835b87e0f013690c30
IgnaDev <arcortoon@gmail.com>
2023-03-08 19:58:26 -0300

    feat: :fire: defini nuestras primeras rutas, ademas aplique un layout importando Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
 import './index.css'
+
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Inicio</h1>
-  },
-  {
-    path: '/nosotros',
-    element: <p>Nosotros</p>
+    element: <Layout/>,
+    children:[ 
+      {
+        index: true,
+        element: <Index/>
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente/>
+      }
+    ]
   }
 ])
 
