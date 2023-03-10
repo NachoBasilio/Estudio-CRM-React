1ec6a55593c6359a19379a221a63f12d895d6684
IgnaDev <arcortoon@gmail.com>
2023-03-10 19:17:32 -0300

    feat: mejore el Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,14 @@ import { Outlet } from "react-router-dom"
 
 export default function Layout() {
   return (
-    <div>
-        <h1 className="text-6xl font-bold">CRM - React</h1>
+    <div className="md:flex md:min-h-screen">
+      <aside className="md:w-1/4 bg-blue-600 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+      </aside>
+
+      <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
         <Outlet/>
+      </main>
     </div>
   )
 }
