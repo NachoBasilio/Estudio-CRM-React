702df5125ded9fbea2d52392eb717fb480ea817e
IgnaDev <arcortoon@gmail.com>
2023-03-10 22:21:50 -0300

    feat: aplicado link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 
 export default function Layout() {
@@ -6,6 +6,10 @@ export default function Layout() {
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-600 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+        <nav className="mt-10">
+            <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Cliente</Link>
+            <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
+        </nav>
       </aside>
 
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
