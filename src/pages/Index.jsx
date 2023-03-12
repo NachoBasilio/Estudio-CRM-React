b0c328237cb34e08dddffc879f38259e591314b1
IgnaDev <arcortoon@gmail.com>
2023-03-12 18:51:07 -0300

    feat: ya se muestran los usuarios por pantalla, esto es gracias a un componente

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import Cliente from "../components/Cliente";
 
 export function loader(){
   const clientes = [
@@ -49,9 +50,29 @@ export default function Index() {
   return (
     <>
     <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
-    <p className="mt-3">Administra tus clientes</p>
-
-    
+    <p className="mt-3">Administra tus clientes</p>  
+    {clientes.length ? (
+      <table className="w-full bg-white shadow mt-5 table-auto">
+        <thead className="bg-blue-800 text-white">
+          <tr>
+            <th className="p-2">Cliente</th>
+            <th className="p-2">Contanto</th>
+            <th className="p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+            
+            {clientes.map(cliente=> (
+              <Cliente
+              cliente={cliente}
+              key={cliente.id}
+              />
+            ))}
+        </tbody>    
+      </table>
+    ):(
+      <p className="text-center mt-10">No hay clientes</p>
+    )}
     </>
   )
 }
