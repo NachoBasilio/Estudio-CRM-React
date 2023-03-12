9cba804775394da7397c45377e8af0a5b6a35973
IgnaDev <arcortoon@gmail.com>
2023-03-11 22:50:47 -0300

    feat: agregue un resaltador en el navegador dependiendo el url

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 
 export default function Layout() {
+  const location = useLocation()
+  console.log(location)
+
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-600 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
         <nav className="mt-10">
-            <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Cliente</Link>
-            <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
+            <Link className={`${location.pathname === "/" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`} to="/">Cliente</Link>
+            <Link className={`${location.pathname === "/clientes/nuevo" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`} to="/clientes/nuevo">Nuevo Cliente</Link>
         </nav>
       </aside>
 
