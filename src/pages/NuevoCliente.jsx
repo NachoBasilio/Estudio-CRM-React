7d6d8b5e0c628e4b088d8b0cf3d8e7d1b1ba389e
IgnaDev <arcortoon@gmail.com>
2023-03-13 17:30:00 -0300

    feat: nuevo cliente tiene estilos y esta todo listo para comenzar con el formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from "react-router-dom"
+
 export default function NuevoCliente() {
+  const navigate = useNavigate()
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Clientes</h1>
+      <p className="mt-3">LLena todos los campos para registrar un nuevo cliente</p>  
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={()=>{navigate(-1)}}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        
+
+      </div>
+    </>
   )
 }
