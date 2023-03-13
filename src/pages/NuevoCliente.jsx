3f69917c1f6cf8e49cc5ebcab2e88a1599a48d91
IgnaDev <arcortoon@gmail.com>
2023-03-13 17:34:18 -0300

    feat: formulario completado, eh de decir que el codigo del componente no es mio pero me acorta tiempo ahora mismo

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario"
 
 export default function NuevoCliente() {
   const navigate = useNavigate()
@@ -16,9 +17,16 @@ export default function NuevoCliente() {
         </button>
       </div>
 
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario></Formulario>
 
+          <input 
+          type="submit" 
+          className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+          value="Registrar Cliente"
+          />
+        </form>
       </div>
     </>
   )
